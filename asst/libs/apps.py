1fb852c8c2bc5ea25d760662a072327c2e9ec837
Yoshioka <sss.yoshioka@gmail.com>
2024-12-17 06:59:26 +0000

    🎸 feat: apply ruff format and rules

--- asst/libs/apps.py
+++ asst/libs/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LibsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'libs'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "libs"
