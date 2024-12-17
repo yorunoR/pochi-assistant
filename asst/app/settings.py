65e74337ab3e155902702a7dd8827f40e97ec8b6
Yoshioka <sss.yoshioka@gmail.com>
2024-12-17 07:53:03 +0000

    🎸 feat: set database

--- asst/app/settings.py
+++ asst/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,15 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME", "asst_dev"),
+        "USER": os.getenv("DATABASE_USER", "developer"),
+        "PASSWORD": os.getenv("USER_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": 5432,
+        "TEST": {
+            "NAME": "asst_test",
+        },
     }
 }
 
@@ -124,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "libs.AdminUser"
