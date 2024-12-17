d2d2eed738e2688d6b630064ebba5dab07793486
Yoshioka <sss.yoshioka@gmail.com>
2024-12-17 15:26:06 +0000

    🎸 feat: graphql exempted from CSRF
    https://strawberry.rocks/docs/breaking-changes/0.243.0

--- asst/app/urls.py
+++ asst/app/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from strawberry.django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 from app.graphql.schema import schema
 from app.views import top
@@ -25,5 +26,5 @@ from app.views import top
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", top, name="top"),
-    path("graphql/", GraphQLView.as_view(schema=schema)),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
 ]
